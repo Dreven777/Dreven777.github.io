44f69cc9487230ea7afe23730e310bf37ec35914
Dreven1~ <dreven7777@gmail.com>
2024-06-09 18:01:18 +0300

    add folder tailwind

---
+++ tailwind/tailwind.config.js
@@ -0,0 +1,14 @@
+tailwind.config = {
+    darkMode: 'selector',
+    theme: {
+      screens: {
+        xs: '320px',
+        sm: '480px',
+        md: '768px',
+        lg: '976px',
+        xl: '1440px',
+      },
+      extend: {
+      }
+    }
+  }
