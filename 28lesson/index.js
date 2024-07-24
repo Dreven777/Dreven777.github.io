0323311e425b2ab8a31dc96aae72fe5e11123c29
Dreven1~ <dreven7777@gmail.com>
2024-07-24 20:35:00 +0300

    update keys

--- 28lesson/index.js
+++ 28lesson/index.js
@@ -3,6 +3,13 @@ const images = document.querySelectorAll('img')
 let currentImage = 0;
 const nextButton = document.getElementById('nextButton');
 const prevButton = document.getElementById('prevButton');
+const pauseButton = document.getElementById('pauseButton');
+let pause = false;
+
+setInterval(() => {
+    if(!pause) nextImage();
+},2000);
+
 
 loadIndicator();
 
@@ -10,14 +17,20 @@ loadIndicator();
 function loadIndicator(){
     if(images.length === 0) return;
     for(let i = 0; i < images.length; i++){
-        const el = document.createElement('span');
-        el.innerHTML = '&bull;'
+        const el = document.createElement('div');
         el.id = `indicator_${i}`;
         el.classList.add('indicator-style');
         slideIndicator.appendChild(el);
     }
     updateKeys();
     updateIndicator();
+    window.addEventListener('keydown', (e) => pushKey(e));
+}
+function pushKey(e){
+    console.log(e.keyCode);
+    if(e.keyCode === 39) nextImage();
+    else if(e.keyCode === 37) prevImage();
+    else if(e.keyCode === 80) pauseSlide();
 }
 
 images.forEach((slide,index)=>{
@@ -37,14 +50,23 @@ function updateIndicator(){
     for(let i = 0; i < images.length; i++){
         const indicator = document.getElementById(`indicator_${i}`);
         if(currentImage === i){
-            indicator.style.color = 'red';
+            indicator.style.background = 'black';
+            indicator.style.scale = '1.2';
         }
         else {
-            indicator.style.color = 'black';
+            indicator.style.background = 'grey';
+            indicator.style.scale = '1';
         }
     }
+    if(!pause) pauseButton.textContent = 'PAUSE';
+    else pauseButton.textContent = 'PLAY';
 }
 
+function pauseSlide() {
+    pause = !pause;
+    if(!pause) pauseButton.textContent = 'PAUSE';
+    else pauseButton.textContent = 'PLAY';
+}
 function updateKeys(){
     prevButton.style.visibility = currentImage <= 0 ? 'hidden' : 'visible';
     nextButton.style.visibility = currentImage >= images.length-1 ? 'hidden' : 'visible';
