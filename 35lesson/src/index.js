047e3f7b182ff57e825a1fffdb9cbc1804b04bd4
Dreven1~ <dreven7777@gmail.com>
2024-08-31 08:33:25 +0300

    webpack 35 lesson

---
+++ 35lesson/src/index.js
@@ -0,0 +1,7 @@
+import "./style.scss";
+import { UserAccount } from "./types.ts";
+
+const newUser = new UserAccount('Oleksandr', 222);
+
+console.log('start');
+console.log(newUser);
---
+++ 35lesson/src/types.ts
@@ -0,0 +1,18 @@
+interface User {
+    name: string;
+    id: number;
+  }
+   
+  class UserAccount {
+    name: string;
+    id: number;
+   
+    constructor(name: string, id: number) {
+      this.name = name;
+      this.id = id;
+    }
+  }
+
+module.exports = {
+  UserAccount
+}
