bc64d4895715e0ae750eb36de31f4a78619a6ddc
Dreven1~ <dreven7777@gmail.com>
2024-09-24 20:15:25 +0300

    42lesson

--- 42lesson/src/App.js
+++ 42lesson/src/App.js
@@ -1,12 +1,81 @@
 
 import './App.css';
+import { BrowserRouter as Router, Route, Routes, Link, NavLink, useNavigate  } from 'react-router-dom';
+import { useEffect } from 'react';
 
 function App() {
+
   return (
     <div className="App">
+      <Router>
+        <NavLink
+            to="/"
+            style={({ isActive }) => {
+              return {
+                fontWeight: isActive ? "bold" : ""
+              };
+            }}
+          >
+            <span className="nav-part">Home</span>
+        </NavLink>
+        <NavLink
+          to="/about"
+          style={({ isActive }) => {
+            return {
+              fontWeight: isActive ? "bold" : ""
+            };
+          }}
+        >
+          <span className="nav-part">About</span>
+      </NavLink>
+      <NavLink
+          to="/contact"
+          style={({ isActive }) => {
+            return {
+              fontWeight: isActive ? "bold" : ""
+            };
+          }}
+        >
+          <span className="nav-part">Contact</span>
+      </NavLink>
+      <NavLink
+          to="/contact/oleksandr"
+          style={({ isActive }) => {
+            return {
+              fontWeight: isActive ? "bold" : ""
+            };
+          }}
+        >
+          <span className="nav-part">Contact Oleksandr</span>
+      </NavLink>
 
+        <Routes>
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="contact/oleksandr" element={<UserContact name="Oleksandr" phone="+332132131231" />} />
+            <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
+}
+
+function Home() {
+  return <h2>
+    Головна сторінка
+    </h2>;
+}
+
+function About() {
+  return <h2>Про нас</h2>;
+}
+
+function Contact() {
+  return <h2>Контакти</h2>;
+}
+
+function UserContact(props) {
+  return <h2>{props.name}<br/>{props.phone}</h2>;
 }
 
 export default App;
