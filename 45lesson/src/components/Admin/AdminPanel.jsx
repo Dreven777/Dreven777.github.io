010200b3e15a5c3b04583ec071dec4b11fc9d070
Dreven1~ <dreven7777@gmail.com>
2024-10-13 14:30:22 +0300

    45lesson async

--- 45lesson/src/components/Admin/AdminPanel.jsx
+++ 45lesson/src/components/Admin/AdminPanel.jsx
@@ -1,9 +1,30 @@
 import styles from './style.module.scss';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getPosts, selectPosts, selectLoading, selectError } from './../../store/async';
+
 
 function AdminPanel(){
+    const dispatch = useDispatch();
+    const posts = useSelector(selectPosts);
+    const loading = useSelector(selectLoading);
+    const error = useSelector(selectError);
+
+    useEffect(() => {
+        dispatch(getPosts());
+      }, [dispatch]);
+    
+      if (loading) return <div>Loading...</div>;
+      if (error) return <div>Error: {error}</div>;
+
     return (
         <div className={styles.main}>
             Панель адміністратора
+            <ul>
+                {posts.map(post => (
+                    <li key={post.id}>{post.title}</li>
+                ))}
+            </ul>
         </div>
     )
 }
--- 45lesson/src/store/store.js
+++ 45lesson/src/store/store.js
@@ -1,8 +1,10 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import userAccessReducer from './userAccess';
+import postReducer from './async';
 
 const rootReducer = combineReducers({
     userAccess: userAccessReducer,
+    posts: postReducer
 });
 
 const store = configureStore({
